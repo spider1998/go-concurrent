7e703940e21a39bd529b45aab1f7c8b8e00ac5a4
刘凡 <2387805574@qq.com>
2019-07-18 14:48:08 +0800

    channel-select

---
+++ 190717/channel-select.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	`fmt`
+	`time`
+)
+
+func main() {
+	var list chan int
+	list = make(chan int,100)
+	go func() {
+		for i := range list{
+			fmt.Print(i)
+		}
+	}()
+
+	go func() {
+		select {
+		case <-list:
+			fmt.Println("insert data ")
+		default:
+			fmt.Println("close....")
+		}
+	}()
+
+	go func() {
+		select {}
+		fmt.Println("waite.....")
+	}()
+
+	for{
+		list <- 1
+		time.Sleep(time.Second*5)
+	}
+}
+
