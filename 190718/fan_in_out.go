72cbde381e96c05215b69b3549aac9d96eb70383
刘凡 <2387805574@qq.com>
2019-07-18 19:03:48 +0800

    扇入、扇出模式

---
+++ 190718/fan_in_out.go
@@ -0,0 +1,143 @@
+package main
+
+import (
+	`fmt`
+	`math/rand`
+	`runtime`
+	`sync`
+	`time`
+)
+
+func main()  {
+
+	toInt := func(done <-chan interface{},valueStream <-chan interface{})<-chan int{
+		intStream := make(chan int)
+		go func() {
+			defer close(intStream)
+			for v := range valueStream{
+				select {
+				case <-done:
+					return
+				case intStream <- v.(int):
+				}
+			}
+		}()
+		return intStream
+	}
+
+	primeFinder := func(done <-chan interface{},valueStream <-chan int)<-chan interface {}{
+		primeStream := make(chan interface{})
+
+
+		ifPrime:= func(n int) bool {
+			if n == 1 {
+			return false
+		}
+			//从2遍历到n-1，看看是否有因子
+			for i := 2; i < n; i++ {
+			if n%i == 0 {
+				//发现一个因子
+				return false
+			}
+		}
+			return true
+		}
+		go func() {
+			defer close(primeStream)
+			for i := range valueStream{
+				/*select {
+				case <-done:
+					return
+				case primeStream <- i:
+				}*/
+				select {
+				case <-done:
+					return
+				default:
+					if ifPrime(i){
+						primeStream <- i
+					}
+				}
+
+			}
+		}()
+		return primeStream
+	}
+
+
+	repeatFn := func(done <-chan interface{},fn func() interface{})<-chan interface {}{
+		valueStream := make(chan interface{})
+		go func() {
+			defer close(valueStream)
+			for {
+				select {
+				case <-done:
+					return
+				case valueStream <- fn():
+				}
+			}
+		}()
+		return valueStream
+	}
+
+
+	take := func(done <- chan interface{}, valueStream <-chan interface{},num int) <-chan interface{}{
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i:=0;i<num;i++{
+				select {
+				case <-done:
+					return
+				case takeStream <- <- valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
+	fanIn := func(done <-chan interface{},channels ...<-chan interface{})<-chan interface {}{
+		var wg sync.WaitGroup
+		multiplexedStream := make(chan interface{})
+		multiplex := func(c <-chan interface{}) {
+			defer wg.Done()
+			for i:= range c{
+				select {
+				case <-done:
+					return
+				case multiplexedStream <- i:
+				}
+			}
+		}
+
+		wg.Add(len(channels))
+		for _,c := range channels{
+			go multiplex(c)
+		}
+
+		go func() {
+			wg.Wait()
+			close(multiplexedStream)
+		}()
+		return multiplexedStream
+	}
+
+	done := make(chan interface{})
+	defer close(done)
+
+	start := time.Now()
+	rands := func() interface{}{return rand.Intn(50000000)}
+	randIntStream := toInt(done,repeatFn(done,rands))
+
+	numFinders := runtime.NumCPU()
+	finders := make([]<-chan interface{},numFinders)
+	for i:=0;i<numFinders;i++{
+		finders[i] = primeFinder(done,randIntStream)
+	}
+
+	for prime := range take(done,fanIn(done,finders...),10){
+		fmt.Printf("\t%d\n",prime)
+	}
+	fmt.Printf("Search took: %v",time.Since(start))
+
+}
