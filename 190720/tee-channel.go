c47eafc8bcdbe53f64e988350de8d5b82dc73161
spider1998 <2387805574@qq.com>
2019-07-20 13:11:21 +0800

    优化返回方法

--- 190720/tee-channel.go
+++ 190720/tee-channel.go
@@ -58,21 +58,21 @@ func main()  {
 		return valsStream
 	}
 
-	tee := func(done,in <-chan interface{})(_ ,_ <-chan interface{}) {
+	tee := func(done,in <-chan interface{})(_ ,_ chan interface{}) {
 		out1 := make(chan interface{})
 		out2 := make(chan interface{})
 		go func() {
 			defer close(out1)
 			defer close(out2)
 			for val := range ordone(done,in){
-				var out1,out2 = out1,out2
+				//var out1,out2 = out1,out2
 				for i:=0;i<2;i++{
 					select {
 					case <-done:
 					case out1 <- val:
-						out1 = nil
+						//out1 = nil
 					case out2 <- val:
-						out2 = nil
+						//out2 = nil
 
 					}
 				}
