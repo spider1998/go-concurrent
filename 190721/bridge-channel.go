60ba2cc9768c6dfc9131f5ae2b564ebae089fe61
spider1998 <2387805574@qq.com>
2019-07-21 21:32:59 +0800

    bridge-channel桥接模式

---
+++ 190721/bridge-channel.go
@@ -0,0 +1,75 @@
+//通过桥接，我们可以在单个range语句中使用处理channel的channel，并专注循环逻辑。
+//（将充满channel的channel拆解为一个简单channel）
+package main
+
+import "fmt"
+
+func main()  {
+
+	ordone := func(done,c <-chan interface{}) <-chan interface {}{
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for{
+				select {
+				case <-done:
+					return
+				case v,ok := <-c:
+					if !ok{
+						return
+					}
+					select {
+					case valStream <- v:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+	bridge := func(done <-chan interface{},chanStream <-chan <-chan interface{}) <-chan interface{}{
+		valStream := make(chan interface{})
+		go func() {
+			defer close(valStream)
+			for{
+				var stream <- chan interface{}
+				select {
+				case <-done:
+					return
+				case maybeStream,ok := <- chanStream:
+					if !ok{
+						return
+					}
+					stream = maybeStream
+				}
+				for val := range ordone(done,stream){
+					select {
+					case valStream <- val:
+					case <-done:
+					}
+				}
+			}
+		}()
+		return valStream
+	}
+
+	genVals := func() <-chan <-chan interface{}{
+		chanStream := make(chan (<-chan interface{}))
+		go func() {
+			defer close(chanStream)
+			for i:=0;i<10;i++{
+				stream := make(chan interface{},1)
+				stream <- i
+				close(stream)
+				chanStream <- stream
+			}
+		}()
+		return chanStream
+	}
+
+	for v := range bridge(nil,genVals()){
+		fmt.Printf("%v ",v)
+	}
+
+}
