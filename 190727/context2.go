7bd2d956fe29e875f4f64eb70e09d38a55231b5e
spider1998 <2387805574@qq.com>
2019-07-27 19:50:27 +0800

    context value

---
+++ 190727/context2.go
@@ -0,0 +1,27 @@
+//存储和检索请求范围数据的Context的数据包
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main()  {
+	ProcessRequest("jane","abc123")
+}
+
+func ProcessRequest(userID,authToken string)  {
+	ctx := context.WithValue(context.Background(),"userID",userID)
+	ctx = context.WithValue(ctx,"authToken",authToken)
+	HanleResponse(ctx)
+}
+
+func HanleResponse(ctx context.Context)  {
+	fmt.Printf(
+		"handlilng response for %v (%v)",
+		ctx.Value("userID"),
+		ctx.Value("authToken"),
+		)
+}
+
+
