3b5dd0040ae9f7dcffacee0722a1f3643b4b5c89
spider1998 <2387805574@qq.com>
2019-07-27 20:02:41 +0800

    context safe type

---
+++ 190727/context3.go
@@ -0,0 +1,40 @@
+//创建类型安全
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+func main()  {
+	ProcessReq("jane","abc123")
+}
+
+type ctxKey int
+
+const (
+	ctxUserID ctxKey = iota
+	ctxToken
+)
+
+func UserID(c context.Context) string {
+	return c.Value(ctxUserID).(string)
+}
+
+func AuthToken(c context.Context) string {
+	return c.Value(ctxToken).(string)
+}
+
+func ProcessReq(userID,authToken string)  {
+	ctx := context.WithValue(context.Background(),ctxUserID,userID)
+	ctx = context.WithValue(ctx,ctxToken,authToken)
+	Handle(ctx)
+}
+
+func Handle(ctx context.Context)  {
+	fmt.Printf(
+		"handle response for %v (ahth: %v)",
+		UserID(ctx),
+		AuthToken(ctx),
+		)
+}
